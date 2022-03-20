e0269cc616c9b20526ad4f5f17aef4cef01f6986
MohammedSB <devhere17@gmail.com>
2022-03-20 23:10:16 +0400

    require guzzle

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,25 +2,24 @@
 
 namespace Devhereco\MalathSMS;
 
-
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
 
-  public function boot()
-  {
-      $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
-      $this->mergeConfigFrom(__DIR__.'/configs/malathsms.php', 'malathsms');
+    public function boot()
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+        $this->mergeConfigFrom(__DIR__.'/configs/malathsms.php', 'malathsms');
 
-      $this->publishes([
-          __DIR__.'/configs/malathsms.php' => config_path('malathsms.php'),
-      ]);
-  }
+        $this->publishes([
+            __DIR__.'/configs/malathsms.php' => config_path('malathsms.php'),
+        ]);
+    }
 
-  public function register()
-  {
-    $this->app->singleton(MalathSMS::class, function () {
-      return new MalathSMS();
-    });
-  }
+    public function register()
+    {
+        $this->app->singleton(MalathSMS::class, function () {
+            return new MalathSMS();
+        });
+    }
 
 }
